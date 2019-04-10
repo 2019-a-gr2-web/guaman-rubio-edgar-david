65d12c37f27ae56b70bd635afc43325de6d787cc
edgardavid2015 <edgardavid2015@users.noreply.github.com>
2019-04-10 16:00:22 -0500

    Métodos get y post

--- 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, HttpCode, Post} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() //Método http
   getHello(): string {
     return this.appService.getHello();
+  }
+  
+  @HttpCode(200)
+
+  @Post() //Método http
+  postHello(): string {
+    return 'Hola mundo en post';
+
   }
 }
