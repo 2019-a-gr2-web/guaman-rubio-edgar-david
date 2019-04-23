273813904f21efdcad8d6e260152ffe9740116da
edgardavid2015 <edgardavid2015@users.noreply.github.com>
2019-04-23 15:53:33 -0500

    Validaciones con Joi, Cookies seguras e inseguras

--- 01-http/02-servidor-web-nodejs/api-web/src/main.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/main.ts
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
+  app.use(cookieParser('Secreto:Me gusta el anime'));
   await app.listen(3000);
 }
 bootstrap();
