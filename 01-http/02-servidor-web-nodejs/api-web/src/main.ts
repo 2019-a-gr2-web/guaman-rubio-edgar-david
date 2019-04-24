1fd1b7db4f20211ef53fce682e037fd3520a8ec3
edgardavid2015 <edgardavid2015@users.noreply.github.com>
2019-04-24 16:00:27 -0500

    Uso de arreglos y sus funciones especificas

--- 01-http/02-servidor-web-nodejs/api-web/src/main.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {join} from "path";
+import {NestExpressApplication} from "@nestjs/platform-express";
 
 const cookieParser = require('cookie-parser');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule) as NestExpressApplication;
   app.use(cookieParser());
+
+  app.setViewEngine('ejs');
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
   await app.listen(3000);
 }
 bootstrap();
